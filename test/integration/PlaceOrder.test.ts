680c2e451554bfd920b9c5ef71b7280d92c47679
Helio Albano de Oliveira <helioalb@gmail.com>
2022-01-04 15:26:43 -0300

    Add coupon repository from database

--- test/integration/PlaceOrder.test.ts
+++ test/integration/PlaceOrder.test.ts
@@ -1,4 +1,3 @@
-import ItemRepositoryMemory from '../../src/infra/repository/memory/ItemRepositoryMemory';
 import OrderRepositoryMemory from '../../src/infra/repository/memory/OrderRepositoryMemory';
 import PlaceOrder from '../../src/application/usecase/place_order/PlaceOrder';
 import PlaceOrderInput from '../../src/application/usecase/place_order/PlaceOrderInput';
@@ -6,10 +5,11 @@ import SimpleFreight from '../../src/domain/entity/SimpleFreight';
 import CouponRepositoryMemory from '../../src/infra/repository/memory/CouponRepositoryMemory';
 import PgPromiseConnectionAdapter from '../../src/infra/database/PgPromiseConnectionAdapter';
 import ItemRepositoryDatabase from '../../src/infra/repository/database/ItemRepositoryDatabase';
+import CouponRepositoryDatabase from '../../src/infra/repository/database/CouponRepositoryDatabase';
 
 test('Place an order', async () => {
     const connection = new PgPromiseConnectionAdapter();
-    const input = new PlaceOrderInput('93541134780', 'DESCONTO10',
+    const input = new PlaceOrderInput('93541134780', 'VALE20',
         [
             { itemId: 1, quantity: 1 },
             { itemId: 2, quantity: 2 },
@@ -20,9 +20,9 @@ test('Place an order', async () => {
 
     const placeOrder = new PlaceOrder(new ItemRepositoryDatabase(connection),
                                       new OrderRepositoryMemory(),
-                                      new CouponRepositoryMemory(),
+                                      new CouponRepositoryDatabase(connection),
                                       new SimpleFreight(1000, 10));
     const output = await placeOrder.execute(input);
     expect(output.code).toBe('202100000001');
-    expect(output.total).toBe(140);
+    expect(output.total).toBe(112);
 });
